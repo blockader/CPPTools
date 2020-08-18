2d1ed93098db4a12190ef7b32fffa01412a275ce
Dongfeng Yu <60870777+blockader@users.noreply.github.com>
2020-08-18 16:57:32 +0800

    Create lru_cache.cpp

---
+++ lru_cache.cpp
@@ -0,0 +1,109 @@
+namespace lru_cache {
+template <class F> struct dense_ {
+    dense_(F function) : f(function) {}
+    template <class V> auto build(int i) const {
+        assert(i == l.size());
+        return numeric_limits<V>::min();
+    }
+    template <class V, class T, class... K>
+    auto build(int i, const T &t, const K &... k) const {
+        auto u = build<V, K...>(i + 1, k...);
+        return vector<decltype(u)>(l[i], u);
+    }
+    template <class M, class V> V &find(M &m) const { return m; }
+    template <class M, class V, class T, class... K>
+    V &find(M &m, const T &t, const K &... k) const {
+        if (t < m.size())
+            return find<decltype(m[t]), V, K...>(m[t], k...);
+        return find<decltype(m[t]), V, K...>(m[e = false], k...);
+    }
+    /*
+    The cache can handle outliers without crashing, but the values won't be
+    saved.
+    */
+    template <class... K> auto operator()(const K &... k) const {
+        typedef decltype(f(*this, k...)) V;
+        typedef decltype(build<V, K...>(0, k...)) M;
+        if (c) {
+            if (p)
+                delete (M *)p, p = 0;
+            c = false;
+        }
+        if (!p)
+            p = new M(move(build<V, K...>(0, k...)));
+        e = true;
+        auto &t = find<M, V, K...>(*((M *)p), k...);
+        return e ? (t == numeric_limits<V>::min() ? t = f(*this, k...) : t)
+                 : f(*this, k...);
+    }
+    F f;
+    static vector<int> l;
+    static void *p;
+    static bool c, e;
+};
+template <class F> vector<int> dense_<F>::l;
+template <class F> void *dense_<F>::p = 0;
+template <class F> bool dense_<F>::c = false;
+template <class F> bool dense_<F>::e = false;
+template <class F> struct sparse_ {
+    sparse_(F function) : f(function) {}
+    template <class V> auto build() const { return V(); }
+    template <class V, class T, class... K>
+    auto build(const T &t, const K &... k) const {
+        return unordered_map<T, decltype(build<V, K...>(k...))>();
+    }
+    template <class M, class V> V &find(M &m) const { return m; }
+    template <class M, class V, class T, class... K>
+    V &find(M &m, const T &t, const K &... k) const {
+        auto i = m.find(t);
+        if (i != m.end())
+            return find<decltype(i->second), V, K...>(i->second, k...);
+        e = false;
+        return find<decltype(i->second), V, K...>(m[t], k...);
+    }
+    template <class... K> auto operator()(const K &... k) const {
+        typedef decltype(f(*this, k...)) V;
+        typedef decltype(build<V, K...>(k...)) M;
+        if (c) {
+            if (p)
+                delete (M *)p, p = 0;
+            c = false;
+        }
+        if (!p)
+            p = new M(move(build<V, K...>(k...)));
+        e = true;
+        auto &t = find<M, V, K...>(*((M *)p), k...);
+        return e ? t : t = f(*this, k...);
+    }
+    F f;
+    static void *p;
+    static bool c, e;
+};
+template <class F> void *sparse_<F>::p = 0;
+template <class F> bool sparse_<F>::e = false;
+template <class F> bool sparse_<F>::c = false;
+/*
+Decorating a function erases all cached values. Please save the resulting
+function if you want to use the old values.
+*/
+struct dense {
+    auto convert() {}
+    template <class T, class... L>
+    auto convert(const T &t, const L &... limits) {
+        l.push_back(t);
+        convert(limits...);
+    }
+    template <class... L> dense(const L &... limits) { convert(limits...); }
+    template <class F> auto operator()(const F &f) const {
+        dense_<F>::l = l, dense_<F>::c = true;
+        return dense_<F>(f);
+    }
+    vector<int> l;
+};
+struct sparse {
+    template <class F> auto operator()(const F &f) const {
+        sparse_<F>::c = true;
+        return sparse_<F>(f);
+    }
+};
+} // namespace lru_cache
