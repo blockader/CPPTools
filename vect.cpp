23dae0d56cf8691dc59ebf929cc9c5e58af0d768
Dongfeng Yu <60870777+blockader@users.noreply.github.com>
2020-08-18 17:59:28 +0800

    Create vect.cpp

---
+++ vect.cpp
@@ -0,0 +1,7 @@
+/*
+The last dimension should be large enough or using vect will be extremely slow.
+*/
+template <class T> auto vect(const T &v, int n) { return vector<T>(n, v); }
+template <class T, class... D> auto vect(const T &v, int n, D... m) {
+    return vector<decltype(vect(v, m...))>(n, vect(v, m...));
+}
